029a4b7c422b781e76d4a2d9908fddccd63a6252
Abid-Rais <abidrais1843@gmail.com>
2021-06-22 15:32:36 -0400

    Two Sum & Two Sum II

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+| No. | Problem Name              | Difficulty | Category 1 | Category 2    |
+| --- | ------------------------- | ---------- | ---------- | ------------- |
+| 1   | Two Sum                   | Easy       | Array      | Hash Table    |
+| 167 | Two Sum II - Sorted Input | Easy       | Array      | Binary Search |
---
+++ two-sum-II.py
@@ -0,0 +1,50 @@
+"""
+    Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+
+    Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+
+    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+    Example 1:
+        Input: numbers = [2,7,11,15], target = 9
+        Output: [1,2]
+        Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+
+    Example 2:
+        Input: numbers = [2,3,4], target = 6
+        Output: [1,3]
+
+    Example 3:
+        Input: numbers = [-1,0], target = -1
+        Output: [1,2]
+    
+    Constraints:
+        2 <= numbers.length <= 3 * 104
+        -1000 <= numbers[i] <= 1000
+        numbers is sorted in non-decreasing order.
+        -1000 <= target <= 1000
+        The tests are generated such that there is exactly one solution.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+            T(n) = O(log n)
+                Binary Search
+            S(n) = O(1)
+                No data structures used 
+        """
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            currSum = numbers[left] + numbers[right]
+            if currSum == target:
+                return [left + 1, right + 1]
+            elif currSum < target:
+                left += 1
+            else:
+                right -= 1
